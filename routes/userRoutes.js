6c72213a7590d9fbf4631f111669de284b0dbb6b
Gerges Samuel <154973597+josamcode@users.noreply.github.com>
2025-01-25 13:35:33 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getUsers, createUser, updateUser, deleteUser } = require("../controllers/userController");
+const { getUsers, createUser, updateUser, deleteUser, getUserById } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUserById);
 router.post("/", createUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
